6afd2832a3777dc702ddab72cad3bec77f02dabe
bagas12dwi <bag12dwi.s@gmail.com>
2022-10-15 02:48:39 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [AuthController::class, 'index'])->name('login')->middleware('guest');
+
+//Route auth
+Route::get('/login', [AuthController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
+
+//Route admin
+Route::group(['middleware' => ['auth', 'ceklevel:admin']], function () {
+    Route::get('/dashboard', [DashboardController::class, 'index']);
+});
