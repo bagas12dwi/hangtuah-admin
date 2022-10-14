aa67aee03112c2c021086d0c663c4f50019ff765
bagas12dwi <bag12dwi.s@gmail.com>
2022-10-15 04:41:14 +0700

    added model and migration

---
+++ database/migrations/2022_10_14_195054_create_testimonials_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('testimonials', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('school_now');
+            $table->text('testimoni');
+            $table->string('type');
+            $table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('testimonials');
+    }
+};
